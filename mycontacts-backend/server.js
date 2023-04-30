387427a23ce2b51365836123921a9afe07bf88a5
Magesh Kumar M <muthumageshkumar@gmail.com>
2023-04-30 19:33:14 +0530

    Router File setup and api's.

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -5,6 +5,9 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+//MiddleWare
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
 app.listen(port, (req, res) => {
   console.log(`Server running on port ${port}`);
 });
