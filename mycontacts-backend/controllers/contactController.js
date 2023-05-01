27d20536de693a4b2d1a5ceb1ce0f33f71735131
Magesh Kumar M <muthumageshkumar@gmail.com>
2023-05-01 17:36:45 +0530

    Created Error Handler and Constants js file.
    Express - Throw Error.
    Error Handling Middleware.

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -13,6 +13,12 @@ const getContacts = (req, res) => {
 //@access public
 
 const createContact = (req, res) => {
+  console.log("This is create contact req", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
   res.status(201).json({ message: "Create contact" });
 };
 
--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorhandler");
 const dorenv = require("dotenv").config();
 
 const app = express();
@@ -6,7 +7,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //MiddleWare
+app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 app.listen(port, (req, res) => {
   console.log(`Server running on port ${port}`);
