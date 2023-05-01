738519ec4303391f9b9af08befbd2a5f99a0780b
Magesh Kumar M <muthumageshkumar@gmail.com>
2023-05-01 18:45:31 +0530

    Mongo Scheme for Contact.

---
+++ mycontacts-backend/models/contactModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+//Create a constant fpe contact scheme which will have value
+
+const contactScheme = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Please add the contact name"],
+    },
+    email: {
+      type: String,
+      require: [true, "Please add the contact email address"],
+    },
+    phone: {
+      type: String,
+      require: [true, "Please add the contact phone number"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Connact", contactScheme);
+//Contact is a model name
