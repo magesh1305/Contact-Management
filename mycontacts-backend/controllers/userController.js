fa0e3ee8229e2e0ff9d6b63e903878f0fd42d723
Magesh Kumar M <muthumageshkumar@gmail.com>
2023-05-01 20:35:42 +0530

    User Registration and Password Hashing.
    npm i bcrypt --> for password hashing.

--- mycontacts-backend/controllers/userController.js
+++ mycontacts-backend/controllers/userController.js
@@ -1,10 +1,34 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 //@desc Register a users
 //@route GET /api/user/register
 //@access public
 
 const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already registered!");
+  }
+
+  //hash password --> going to store in db so that we can secure the user password
+  const hashPassword = await bcrypt.hash(password, 10);
+  console.log("Hashed Password:", hashPassword);
+  const user = await User.create({ username, email, password: hashPassword });
+  console.log(`user created ${user}`);
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
   res.json({ message: "Register the user" });
 });
 
